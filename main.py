6c4c4dafac1ce7a814416ab5f06b13f00407b0e0
DVR <135457531+dangluva@users.noreply.github.com>
2023-11-03 23:40:55 +0200

    Added erase all widgets info function
    When the user presses the total bill for the second time, all the data that is located in other widgets is erased

--- main.py
+++ main.py
@@ -8,22 +8,23 @@ from kivy.uix.button import Button
 class TipCalculatorApp(App):
     def build(self):
         self.title = "Tip Calculator"
+        self.calculate_pressed = 0  # Track the number of times "Calculate" button was pressed
 
         layout = GridLayout(cols=2, spacing=10, padding=10)
 
-        layout.add_widget(Label(text="Total Bill:"))
-        self.bill_input = TextInput(hint_text="Enter total bill", multiline=False)
+        layout.add_widget(Label(text="Total Bill:", color='green', bold=True))
+        self.bill_input = TextInput(hint_text="Enter total bill", input_type='number', multiline=False, on_touch_down=self.clear_data)
         layout.add_widget(self.bill_input)
 
-        layout.add_widget(Label(text="Tip Percentage:"))
-        self.tip_input = TextInput(hint_text="Enter tip percentage", multiline=False)
+        layout.add_widget(Label(text="Tip Percentage:", color='pink', bold=True))
+        self.tip_input = TextInput(hint_text="Enter tip percentage", input_type='number', multiline=False)
         layout.add_widget(self.tip_input)
 
-        layout.add_widget(Label(text="Number of People:"))
-        self.split_input = TextInput(hint_text="Enter number of people", multiline=False)
+        layout.add_widget(Label(text="Number of People:", color='red', bold=True))
+        self.split_input = TextInput(hint_text="Enter number of people", input_type='number', multiline=False)
         layout.add_widget(self.split_input)
 
-        calculate_button = Button(text="Calculate")
+        calculate_button = Button(text="Calculate", color='yellow', bold=True, font_size=20, background_normal='', background_color='blue')
         calculate_button.bind(on_press=self.calculate_tip)
         layout.add_widget(calculate_button)
 
@@ -46,6 +47,16 @@ class TipCalculatorApp(App):
 
         except (ValueError, ZeroDivisionError):
             self.result_label.text = "Please enter valid values."
+
+    def clear_data(self, instance, touch):
+        if self.bill_input.collide_point(*touch.pos):
+            self.bill_input.text = ''
+            self.tip_input.text = ''
+            self.split_input.text = ''
+            self.result_label.text = ''
+            self.calculate_pressed = 0
+        else:
+            self.calculate_pressed += 1
 
 
 if __name__ == '__main__':
